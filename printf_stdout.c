56d141ca3c5039e5fb6a977d6eebf64bec68f192
Munene <muneneemasonmutuma@gmail.com>
2022-04-19 17:12:29 +0300

    0.2 handle space betweeen % and s/c

--- printf_stdout.c
+++ printf_stdout.c
@@ -3,6 +3,7 @@
 
 int str_print(char *string);
 int character(char c);
+int zero_space(char *str);
 
 /**
  * _printf - produces output according to a formart
@@ -13,37 +14,40 @@ int character(char c);
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i, len;
+	int i, k, len;
 	char *str;
 
 	va_start(ap, format);
-
 	len = 0;
 	if (!format)
-	{
 		return (0);
-	}
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] == 's')
+		k = 1;
+		if (format[i] == '%' && format[i + 1] == ' ')
+		{
+			str = (char *)(format + i + 1);
+			k += zero_space(str);
+		}
+		if (format[i] == '%' && format[i + k] == 's')
 		{
 			str = va_arg(ap, char *);
 			len += str_print(str);
-			i += 2;
+			i += k + 1;
 		}
 
-		if (format[i] == '%' && format[i + 1] == 'c')
+		if (format[i] == '%' && format[i + k] == 'c')
 		{
 			len += character(va_arg(ap, int));
-			i += 2;
+			i += k + 1;
 		}
 
-		if (format[i] == '%' && format[i + 1] == '%')
+		if (format[i] == '%' && format[i + k] == '%')
 		{
 			_putchar('%');
 			len++;
-			i += 1;
+			i += k;
 		}
 
 		else
@@ -87,4 +91,20 @@ int character(char c)
 {
 	_putchar(c);
 	return (1);
+}
+
+/**
+ * zero_space - counts number of consecutive whitespaces
+ *
+ * @str: string that starts with whitespace
+ *
+ * Return: Number of consecutive whitespaces
+ */
+int zero_space(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] == ' '; i++)
+		;
+	return (i);
 }
