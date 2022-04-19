38dfb2b4696df39bc7fe58ca8436f73da1a1a011
Munene <muneneemasonmutuma@gmail.com>
2022-04-19 12:06:17 +0300

    0  printf  char and string:

--- printf_stdout.c
+++ printf_stdout.c
@@ -10,14 +10,40 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int i;
+	int i, j, len;
+	char *str;
 
-	va_start(ap, formart);
+	va_start(ap, format);
 
-	for (formart[i] = 0; formart[i] != '%s'; i++);
+	len = 0;
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		_putchar(formart[i]);
+		if (format[i] == '%' && format[i + 1] == 's')
+		{
+			str = va_arg(ap, char *);
+
+			for (j = 0; str[j] != '\0'; j++)
+			{
+				_putchar(str[j]);
+				len++;
+			}
+			i += 2;
+		}
+
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			_putchar(va_arg(ap, int));
+			len++;
+			i++;
+		}
+
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
 	}
 
 	va_end(ap);
+	return (len);
 }
